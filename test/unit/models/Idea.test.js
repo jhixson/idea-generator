860010da53ed84d0642a8d1639fe9b281309046e
Jesse Hixson <hixsonj@gmail.com>
2014-09-26 14:19:42 -0400

    Adds idea model/controller with basic validation test. Adds recursive option for mocha.

---
+++ test/unit/models/Idea.test.js
@@ -0,0 +1,12 @@
+describe.only('IdeaModel', function() {
+
+  describe('#create()', function() {
+    it('should be invalid without a name', function (done) {
+      Idea.create().exec(function(err, idea) {
+        should.exist(err);
+        should.not.exist(idea);
+        done();
+      })
+    });
+  });
+});
